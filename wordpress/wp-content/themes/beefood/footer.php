3fa9287aafc642c11b98878867be91cd0b8876be
Josue-U <josueulliana@gmail.com>
2021-05-17 14:50:06 +0200

    creation footer.php & header.php

---
+++ wordpress/wp-content/themes/beefood/footer.php
@@ -0,0 +1,57 @@
+<?php wp_footer(); ?>
+
+<div class="top-footer-container">
+    <div class="join-news-letter">
+        <h2>JOIN OUR NEWSLETTER</h2>
+        <input type="text" value="Your Email Address"/>
+        <button>SUBSCRIBE</button>
+    </div>
+</div>
+
+<div class="middle-footer-container">
+    <div class="dev-restaurant">
+        <h2>Dev Restaurant</h2>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+        Repellat sit omnis molestiae cupiditate odio quasi! Laborum, dignissimo.</p>
+
+        <div class="socials-links">
+            <a href="https://fr-fr.facebook.com/">facebook</a>
+            <a href="https://twitter.com/?lang=fr">twitter</a>
+            <a href="https://www.pinterest.fr/">pinterest</a>
+            <a href="https://www.linkedin.com/">linkedin</a>
+        </div>
+    </div>
+
+    <div class="open-hours">
+        <h3>Open Hours</h3>
+        <ul>
+            <li><p>Mondays _______ closed</p></li>
+            <li><p>Tue-Fri _______ 10am - 12am</p></li>
+            <li><p>Sat-Sun _______ /am - 1am</p></li>
+            <li><p>Public Holidays _______ /am - 1am</p></li>
+        </ul>
+        
+    </div>
+
+    <div class="contact-us">
+        <h3>Contact Us</h3>
+        <ul>
+            <li>+1(123) xxx-xxxx</li>
+            <li>Rue de Mulhouse, 36 4020 Liège Belgium</li>
+            <li>odewdesign.sutio@gmail.com</li>
+        </ul>
+    </div>
+
+    <div class="instagram">
+        <h3>Instagram</h3>
+        <p>photo</p>
+    </div>
+
+    <div class="bottom-footer-container">
+        <p>2021 All Rights Reserved By BeCode</p>
+    </div>
+    
+</div>
+
+</body>
+</html>
