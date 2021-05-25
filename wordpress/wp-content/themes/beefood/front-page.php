2027150e0eb65f45deab0d1a75e1ee3e2728e07b
Dhaibuna <nataschagritte@gmail.com>
2021-05-26 01:01:56 +0200

    add one card

--- wordpress/wp-content/themes/beefood/front-page.php
+++ wordpress/wp-content/themes/beefood/front-page.php
@@ -1,13 +1,11 @@
-<?php get_header(); ?>
-
-
 <?php
 //Je vais récuépérer les données avec get_field, et je les affiche dans une variable
     $background_image = get_field("background_image");
-    
 ?>
 
 <section style="background-image: url('<?php echo $background_image['url']; ?>')">
+<?php get_header(); ?>
+
 <!--<img href="http://localhost/wp-content/uploads/2021/05/banner-top.jpg"/> -->
     <div>
         <h1>
@@ -16,6 +14,13 @@
         <h1>
     </div>
     <!-- Faire 3 cards ici pour inclure les champs des icones remplies dans le dashboard --> 
+    <div class="card" style="width: 18rem;">
+  <img class="card-img-top" src="..." alt="Card image cap">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+  </div>
+</div>
 </section>
 
 <?php
