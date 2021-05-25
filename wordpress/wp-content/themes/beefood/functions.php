88215feabf437b497dc6efc8502e29b840dc5e8c
Josue-U <josueulliana@gmail.com>
2021-05-25 16:48:50 +0200

    add bootstrap

--- wordpress/wp-content/themes/beefood/functions.php
+++ wordpress/wp-content/themes/beefood/functions.php
@@ -11,7 +11,9 @@ function beefood_init()
 
 function add_styles()
 {
-    
+    wp_register('stylesheet', get_template_directory_uri() . '/css/bootstrap.min.css', array(), false, 'all');
+    wp_enqueue_style('stylesheet');
+
     wp_register_style('style', get_template_directory_uri() . "/style.css", array(), false, "all");
     wp_enqueue_style('style');
 
