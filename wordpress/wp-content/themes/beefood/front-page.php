641d5bea2e40dd67c47ec416eeab7bb22ed8a78a
Dhaibuna <nataschagritte@gmail.com>
2021-05-26 11:29:27 +0200

    add josue modifications

--- wordpress/wp-content/themes/beefood/front-page.php
+++ wordpress/wp-content/themes/beefood/front-page.php
@@ -1,39 +1,64 @@
 <?php
 //Je vais récuépérer les données avec get_field, et je les affiche dans une variable
-    $background_image = get_field("background_image");
+$background_image = get_field("background_image");
 ?>
 
 <section style="background-image: url('<?php echo $background_image['url']; ?>')">
-<?php get_header(); ?>
+    <?php get_header(); ?>
 
-<!--<img href="http://localhost/wp-content/uploads/2021/05/banner-top.jpg"/> -->
+    <!--<img href="http://localhost/wp-content/uploads/2021/05/banner-top.jpg"/> -->
     <div>
         <h1>
             <?php the_field("subtitle"); ?> </br>
             <?php the_field("main_title"); ?>
-        <h1>
+            <h1>
     </div>
-    <!-- Faire 3 cards ici pour inclure les champs des icones remplies dans le dashboard --> 
+    <!-- Faire 3 cards ici pour inclure les champs des icones remplies dans le dashboard -->
     <div class="card" style="width: 18rem;">
-  <img class="card-img-top" src="..." alt="Card image cap">
-  <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-  </div>
-</div>
+        <img class="card-img-left" src="assets/svg/quality-food.svg" alt="Card image cap">
+        <div class="card-body">
+            <h5 class="card-title">QUALITY FOODS</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        </div>
+    </div>
+
+    <div class="card" style="width: 18rem;">
+        <img class="card-img-middle" src="assets/svg/fastest-delivery.svg" alt="Card image cap">
+        <div class="card-body">
+            <h5 class="card-title">FASTEST DELIVERY</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        </div>
+    </div>
+
+    <div class="card" style="width: 18rem;">
+        <img class="card-img-right" src="assets/svg/original-recipes.svg" alt="Card image cap">
+        <div class="card-body">
+            <h5 class="card-title">ORIGINAL RECIPES</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        </div>
+    </div>
 </section>
 
-<?php
+<section class="container">
+    <div class="row">
+        <div class="col-6"></div>
+        <div class="col-6">test</div>
+    </div>
+</section>
 
-   /* if( have_posts() ){
+<?php if (have_posts()) : ?>
 
-        while( have_posts() ){
-         // Will execute this as lon as there is a post
-            the_post();
-            the_content();
-        }
-    } */
+    <ul>
+        <?php while (have_posts()) : the_post(); ?>
+            <li></li>
+        <?php endwhile ?>
+    </ul>
 
-?>
+
+<?php else : ?>
+    <h1>Pas d'article</h1>
+
+<?php endif; ?>
+
 
 <?php get_footer(); ?>
