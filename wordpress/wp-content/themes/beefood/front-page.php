0ffca0c13d486ed8e15b9812bdd60469f97af96a
Josue-U <josueulliana@gmail.com>
2021-05-26 08:33:58 +0200

    litle ident modif

--- wordpress/wp-content/themes/beefood/front-page.php
+++ wordpress/wp-content/themes/beefood/front-page.php
@@ -1,42 +1,42 @@
 <?php
 //Je vais récuépérer les données avec get_field, et je les affiche dans une variable
-    $background_image = get_field("background_image");
+$background_image = get_field("background_image");
 ?>
 
 <section style="background-image: url('<?php echo $background_image['url']; ?>')">
-<?php get_header(); ?>
+    <?php get_header(); ?>
 
-<!--<img href="http://localhost/wp-content/uploads/2021/05/banner-top.jpg"/> -->
+    <!--<img href="http://localhost/wp-content/uploads/2021/05/banner-top.jpg"/> -->
     <div>
         <h1>
             <?php the_field("subtitle"); ?> </br>
             <?php the_field("main_title"); ?>
-        <h1>
+            <h1>
     </div>
-    <!-- Faire 3 cards ici pour inclure les champs des icones remplies dans le dashboard --> 
+    <!-- Faire 3 cards ici pour inclure les champs des icones remplies dans le dashboard -->
     <div class="card" style="width: 18rem;">
-  <img class="card-img-top" src="..." alt="Card image cap">
-  <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-  </div>
-</div>
+        <img class="card-img-top" src="..." alt="Card image cap">
+        <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        </div>
+    </div>
 </section>
 
 
-<?php if(have_posts()): ?>
+<?php if (have_posts()) : ?>
 
     <ul>
-        <?php while(have_posts()): the_post(); ?>
+        <?php while (have_posts()) : the_post(); ?>
             <li></li>
         <?php endwhile ?>
     </ul>
-    
 
-<?php else: ?>
+
+<?php else : ?>
     <h1>Pas d'article</h1>
 
-<?php endif; ?>    
+<?php endif; ?>
 
 
 <?php get_footer(); ?>
