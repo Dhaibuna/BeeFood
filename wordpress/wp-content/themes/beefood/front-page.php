a6a2b5073e1aa7b139b26cc7240e7bd30b0a2b37
Dhaibuna <nataschagritte@gmail.com>
2021-05-26 12:11:41 +0200

    update bootstraap card size

--- wordpress/wp-content/themes/beefood/front-page.php
+++ wordpress/wp-content/themes/beefood/front-page.php
@@ -7,36 +7,45 @@ $background_image = get_field("background_image");
     <?php get_header(); ?>
 
     <!--<img href="http://localhost/wp-content/uploads/2021/05/banner-top.jpg"/> -->
-    <div>
-        <h1>
-            <?php the_field("subtitle"); ?> </br>
-            <?php the_field("main_title"); ?>
+        <div>
             <h1>
-    </div>
-    <!-- Faire 3 cards ici pour inclure les champs des icones remplies dans le dashboard -->
-    <div class="card" style="width: 18rem;">
-        <img class="card-img-left" src="http://localhost/wp-content/uploads/2021/05/quality-food.svg" alt="Card image cap">
-        <div class="card-body">
-            <h5 class="card-title">QUALITY FOODS</h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <?php the_field("subtitle"); ?> </br>
+                <?php the_field("main_title"); ?>
+                <h1>
         </div>
-    </div>
 
-    <div class="card" style="width: 18rem;">
-        <img class="card-img-middle" src="http://localhost/wp-content/uploads/2021/05/fastest-delivery.svg" alt="Card image cap">
-        <div class="card-body">
-            <h5 class="card-title">FASTEST DELIVERY</h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <!-- Icon cards -->
+    <div class="row">
+        <div class="col">
+            <div class="card" style="width: 15rem; height: 15rem;">
+                <img class="card-img-left" src="http://localhost/wp-content/uploads/2021/05/quality-food.svg" alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title">QUALITY FOODS</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+            </div>
+        </div>
+        <div class="col">
+            <div class="card" style="width: 15rem; height: 15rem; ">
+                <img class="card-img-middle" src="http://localhost/wp-content/uploads/2021/05/fastest-delivery.svg" alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title">FASTEST DELIVERY</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+            </div>
+        </div>
+        <div class="col">
+            <div class="card" style="width: 15rem; height: 15rem;">
+                <img class="card-img-right" src="http://localhost/wp-content/uploads/2021/05/original-recipes.svg" alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title">ORIGINAL RECIPES</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+            </div>
         </div>
     </div>
+    <!-- Icon cards -->
 
-    <div class="card" style="width: 18rem;">
-        <img class="card-img-right" src="http://localhost/wp-content/uploads/2021/05/original-recipes.svg" alt="Card image cap">
-        <div class="card-body">
-            <h5 class="card-title">ORIGINAL RECIPES</h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        </div>
-    </div>
 </section>
 
 <section class="container">
